938741233f339f5cbeef82cdbbc47ce1b832f9bf
Víctor Hugo Gallegos Mota <316160456@ciencias.unam.mx>
2023-04-24 22:35:08 -0600

    FIX Bug 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://VictorDeGallegos.github.io/',
+  base: 'https://victordegallegos.github.io/gh-actions-proof/',
   plugins: [react()],
 })
